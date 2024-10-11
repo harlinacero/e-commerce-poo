7164ff9996656601c24f15ef498c4ccaa2f83ced
SIIGO\harlin.acero <acer803372@siigo.com>
2024-10-11 00:47:04 -0500

    Método main y funciones adicionales

---
+++ README.md
@@ -0,0 +1,42 @@
+
+- **.vscode/**: Contiene configuraciones específicas del entorno de desarrollo Visual Studio Code.
+  - `launch.json`: Configuración para depuración.
+- **domain/**: Contiene los archivos de dominio del proyecto.
+  - `product.go`: Define la estructura y métodos relacionados con los productos.
+  - `shoppingCar.go`: Define la estructura y métodos relacionados con el carrito de compras.
+  - `user.go`: Define la estructura y métodos relacionados con los usuarios.
+- **go.mod**: Archivo de módulos de Go que define las dependencias del proyecto.
+- **main.go**: Archivo principal que contiene la lógica para interactuar con el usuario y gestionar productos.
+
+## Ejecución del Proyecto
+
+Para ejecutar el proyecto, sigue estos pasos:
+
+1. **Clonar el repositorio**:
+    ```sh
+    git clone <URL_DEL_REPOSITORIO>
+    cd <NOMBRE_DEL_REPOSITORIO>
+    ```
+
+2. **Instalar dependencias**:
+    ```sh
+    go mod tidy
+    ```
+
+3. **Ejecutar la aplicación**:
+    ```sh
+    go run main.go
+    ```
+
+## Uso
+
+Al ejecutar la aplicación, se te pedirá que introduzcas la información del producto:
+
+1. Nombre del producto
+2. Descripción del producto
+3. Valor bruto del producto
+4. Porcentaje de impuesto
+5. Porcentaje de descuento
+6. Cantidad de stock
+
+La aplicación calculará y mostrará los valores correspondientes al impuesto, descuento y valor real del producto.
--- main.go
+++ main.go
@@ -1,51 +1,144 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 
 	"ecommerce.com/ecommerce/domain"
 )
 
 func main() {
-	fmt.Println("Bienvenido a la clase de POO")
+	reader := bufio.NewReader(os.Stdin)
+	var shoppingCar domain.ShoppingCar
 
-	addProduct()
+	fmt.Println("Bienvenido al ecommerce Majestic")
+	user, err := addUser()
+	if err != nil {
+		fmt.Println("Error al crear el usuario", err)
+		return
+	}
 
+	fmt.Printf("%s Desea agregar un producto? (s/n)", user.GetUsername())
+	addProduct, _ := reader.ReadString('\n')
+	addProduct = strings.TrimSpace(addProduct)
 
+	for addProduct == "s" {
+		product := addNewProduct()
+
+		fmt.Println("Desea agregar el producto al carrito? (s/n)")
+
+		addProductToShoppingCar, _ := reader.ReadString('\n')
+		addProductToShoppingCar = strings.TrimSpace(addProductToShoppingCar)
+
+		if addProductToShoppingCar == "s" {
+			shoppingCar.AddProduct(*product)
+		}
+
+		fmt.Printf("%s Desea agregar otro producto? (s/n)", user.GetUsername())
+		addProduct, _ = reader.ReadString('\n')
+		addProduct = strings.TrimSpace(addProduct)
+	}
+
+	showShoppingCar(shoppingCar)
 }
 
-func addProduct() {
+func showShoppingCar(shoppingCar domain.ShoppingCar) {
+	fmt.Println("******************************************************************")
+	fmt.Println("Usted ha agregado los siguientes productos a su carrito de compras")
+	for i, product := range shoppingCar.Productcs {
+		fmt.Println("************ Producto ", i+1, " ************")
+		fmt.Println("Nombre: ", product.GetName())
+		fmt.Println("Descripción: ", product.GetDescription())
+		fmt.Println("Valor Bruto: ", product.GetGrossValue())
+		fmt.Println("Impuesto: ", product.GetTaxAddValue())
+		fmt.Println("Descuento: ", product.GetDiscValue())
+		fmt.Println("Valor Real: ", product.GetRealValue())
+	}
+
+	fmt.Println("******************************************************************")
+	fmt.Println("Total de la Compra: ", shoppingCar.CalculateTotalValue())
+}
+
+func addUser() (*domain.User, error) {
+	var user *domain.User
 	var name string
-	var description string
-	var grossvalue float64
-	var taxpercentaje float64
-	var discpercentaje float64
-	var stock int
+	var email string
+	var password string
+	var password2 string
+	var address string
 
-	fmt.Println("Introduce el nombre del producto")	
+	fmt.Println("Para iniciar, ingresa tus datos")
+	fmt.Println("Nombre: ")
 	fmt.Scanln(&name)
+	fmt.Println("Email: ")
+	fmt.Scanln(&email)
+	fmt.Println("Contraseña: ")
+	fmt.Scanln(&password)
+	fmt.Println("Confirma tu contraseña: ")
+	fmt.Scanln(&password2)
+
+	if password != password2 {
+		return user, errors.New("Contraseñas no coinciden")
+	}
+
+	fmt.Println("Dirección: ")
+	fmt.Scanln(&address)
+
+	user = domain.NewUser(name, name, email, password, address)
+	username := user.GetUsername()
+	fmt.Printf("El usuario %s ha sido creado", username)
+	fmt.Println("")
+	return user, nil
+}
+
+func addNewProduct() *domain.Product {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("******************************************************************")
+	fmt.Println("Introduce el nombre del producto")
+	name, _ := reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+
 	fmt.Println("Introduce una descripcion")
-	fmt.Scanln(&description)
+	description, _ := reader.ReadString('\n')
+	description = strings.TrimSpace(description)
+
 	fmt.Println("Introduce el valor bruto del producto")
-	fmt.Scanln(&grossvalue)
+	grossvalueStr, _ := reader.ReadString('\n')
+	grossvalueStr = strings.TrimSpace(grossvalueStr)
+	grossvalue, _ := strconv.ParseFloat(grossvalueStr, 64)
+
 	fmt.Println("Introduce el porcentaje de impuesto")
-	fmt.Scanln(&taxpercentaje)
+	taxpercentajeStr, _ := reader.ReadString('\n')
+	taxpercentajeStr = strings.TrimSpace(taxpercentajeStr)
+	taxpercentaje, _ := strconv.ParseFloat(taxpercentajeStr, 64)
+
 	fmt.Println("Introduce el porcentaje de descuento")
-	fmt.Scanln(&discpercentaje)
+	discpercentajeStr, _ := reader.ReadString('\n')
+	discpercentajeStr = strings.TrimSpace(discpercentajeStr)
+	discpercentaje, _ := strconv.ParseFloat(discpercentajeStr, 64)
+
 	fmt.Println("Introduce la cantidad de stock")
-	fmt.Scanln(&stock)
+	stockStr, _ := reader.ReadString('\n')
+	stockStr = strings.TrimSpace(stockStr)
+	stock, _ := strconv.Atoi(stockStr)
 
+	product := domain.NewProduct(name, name, description, grossvalue, discpercentaje, taxpercentaje, stock)
 
-	product1 := domain.NewProduct(name, name, description, grossvalue, discpercentaje, taxpercentaje, stock)
+	fmt.Println(product.GetName())
 
-	fmt.Println(product1.GetName())
+	grossvalue = product.GetGrossValue()
+	taxvalue := product.GetTaxAddValue()
+	discvalue := product.GetDiscValue()
+	realvalue := product.GetRealValue()
 
-    grossvalue = product1.GetGrossValue()
-	taxvalue := product1.GetTaxAddValue()
-	discvalue := product1.GetDiscValue()
-	realvalue := product1.GetRealValue()
-	fmt.Println("Gross Value: ", grossvalue)
-	fmt.Println("Tax Value: ", taxvalue)
-	fmt.Println("Disc Value: ", discvalue)
-	fmt.Println("Real Value: ", realvalue)
+	fmt.Println("Valor Bruto: ", grossvalue)
+	fmt.Println("Impuestos: ", taxvalue)
+	fmt.Println("Decuentos: ", discvalue)
+	fmt.Println("Total: ", realvalue)
+
+	return product
 }
